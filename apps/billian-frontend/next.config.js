cb4d3e349df5c0d4e67988e5bbce286a413ca2ad
Kade Illian <kadeillianmt@gmail.com>
2023-09-07 19:15:54 +0000

    fixed lint, but enviromental variables still aren't being read

--- apps/billian-frontend/next.config.js
+++ apps/billian-frontend/next.config.js
@@ -1,14 +1,37 @@
-/** @type {import('next').NextConfig} */
+import {PrismaPlugin} from '@prisma/nextjs-monorepo-workaround-plugin'
 import {configEnv} from "../../packages/config/env/src/env.nextjs.mjs"
 
-const { serverConfig, publicConfig } = configEnv()
+const {serverConfig,publicConfig} = configEnv()
 
-const nextConfig = {
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
   serverRuntimeConfig: serverConfig,
   publicRuntimeConfig: publicConfig,
+  /** Enables hot reloading for local packages without a build step */
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   images: {
     domains: ['tailwindui.com', 'images.unsplash.com']
-  }
-}
-
-module.exports = nextConfig
+  },
+  // eslint: { ignoreDuringBuilds: !!process.env.CI },
+  // typescript: { ignoreBuildErrors: !!process.env.CI },
+  /**
+   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+   * out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
+};
+export default config;
--- packages/config/env/src/env.nextjs.mjs
+++ packages/config/env/src/env.nextjs.mjs
@@ -1,18 +1,22 @@
-import dotenv from "dotenv-vault";
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import dotenv from "dotenv-vault"
 
 export const configEnv = () => {
-  const serverConfig = dotenv.config({path: '../../.env'}).parsed ?? dotenv.config({path: '../../.env.vault'}).parsed ?? process.env;
+  const serverConfig = dotenv.config({path: '../../.env'}).parsed ?? dotenv.config({path: '../../.env.vault'}).parsed ?? process.env
   const publicConfig = Object.keys(serverConfig)
     .filter((key) => key.startsWith('NEXT_PUBLIC'))
     .reduce((obj, key) => {
       return Object.assign(obj, {
         [key]: serverConfig[key]
-      });
-    }, {});
+      })
+    }, {})
   if (serverConfig?.DOTENV_VAULT) {
-    console.error("ERROR ACCESSING DOTENV VAULT:  Is DOTENV_KEY set?");
+    console.error("ERROR ACCESSING DOTENV VAULT:  Is DOTENV_KEY set?")
     // console.log(`${JSON.stringify(serverConfig, null, 2)} - serverConfig`)
   }
   // console.log(`${JSON.stringify(publicConfig, null, 2)} - publicConfig`)
-  return {serverConfig, publicConfig};
-};
+  return {serverConfig, publicConfig}
+}
